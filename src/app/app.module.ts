36167ab49e2903347e22818409eecd1775a977bc
Jeferson <jefersonsilvacomp@gmail.com>
2020-02-28 16:41:32 -0300

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,9 @@
 import { NgModule } from '@angular/core';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSliderModule } from '@angular/material/slider';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatListModule} from '@angular/material/list';
-import {MatButtonModule} from '@angular/material/button';
 import { ContactModule } from './components/contact/contact.module';
-
-
 
 
 @NgModule({
@@ -21,13 +13,8 @@ import { ContactModule } from './components/contact/contact.module';
     BrowserModule,
     ContactModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSliderModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatListModule,
-    MatButtonModule
+    BrowserAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
